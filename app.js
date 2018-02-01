1593df411e8ad8c25003fa3f184db727a8d916fc
Hong Sung <profsung@gmail.com>
2018-01-31 21:54:43 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// session
+
+const express = require('express');
+const session = require('express-session');
+const app = express();
+
+app.use(session({
+    secret: 'mysecretcookie',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {maxAge: 60000}   // if inactive, session expires in 1 hour
+}));
+
+app.get('/', (req, res) => {
+	console.log(req.session);
+	if (!req.session.count) {
+		req.session.count = 1;
+	} else {
+		req.session.count += 1;
+	}
+	res.send('done!');
+});
+app.get('/test', (req, res) => {
+	console.log(req.session);
+	if (!req.session.count) {
+		req.session.count = 1;
+	} else {
+		req.session.count += 1;
+	}
+	res.send('done!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server is running at port', port);
+});
